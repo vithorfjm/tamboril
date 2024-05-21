74e2141296928a41375cce987191298514875e06
Vithor Félix <vithorfjm@outlook.com>
2024-05-20 23:46:35 -0300

    update application.properties

---
+++ src/main/java/br/com/tamboril/domain/order/Order.java
@@ -0,0 +1,10 @@
+package br.com.tamboril.domain.order;
+
+import jakarta.persistence.*;
+
+public class Order {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+}
---
+++ src/main/java/br/com/tamboril/repositories/CategoryRepository.java
@@ -0,0 +1,4 @@
+package br.com.tamboril.repositories;
+
+public class CategoryRepository {
+}
